5a1d0a8f59407f79f416ddc4f29095df2545d563
cromeo98 <romeo.christian98@gmail.com>
2021-06-18 15:21:32 +0200

    add: solution 1 (foreach)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 // Usiamo i nuovi metodi degli array foreach o filter.
+//1) creo l'array 
+const myArray = ['a', 1, 'b', 2, 'c', 3, 'd', 4];
+
+//3) richiamo la funzione
+console.log(returnNewArr(myArray, 3, 6));
+
+//*********** Functions */
+//2a) scrivo la funzione che può contenere i tre argomenti (FOREACH)
+function returnNewArr(arr, min, max){
+    const newArray = [];
+    arr.forEach((element, index, arr) => {
+        if(index >= min && index <= max){
+            newArray.push(element); //arr[index]
+        }
+    
+    });
+    return newArray;
+}
